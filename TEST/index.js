1210aef0886f6d950caf49307324b329cab2675c
ankitkumar764 <ankit.kr.cg@gmail.com>
2026-03-15 21:34:58 +0530

    again3

--- TEST/index.js
+++ TEST/index.js
@@ -42,7 +42,11 @@ const UserSchema = new mongoose.Schema({
     // isActive:{
     //     Boolean: true
     // }
-})
+},
+    {
+        versionKey: false
+    }
+)
 
 
 const Collection = mongoose.model("Coll1", UserSchema);
@@ -85,24 +89,131 @@ app.get('/students/course/:courseName', async (req, res) => {
 })
 
 
-app.put('/students/course/:courseName',async(req,res)=>{
-    const crsd=(req.params.courseName);
-    const updated=req.body;
-    const result=await Collection.replaceOne({course:crsd},updated);
+app.put('/students/course/:courseName', async (req, res) => {
+    const crsd = (req.params.courseName);
+    const updated = req.body;
+    const result = await Collection.replaceOne({ course: crsd }, updated);
     res.json(result);
 })
 
 
 
 // PATCH_KE_THROUGH_SIRF_EK_FIELD_KO_POSTMAN_MEIN_LIKHNE_KI_ZARURAT_HAI_{name:RAVI}
-app.patch('./students/cousre/:courseName',async(req,res)=>{
-    const coursed =req.params.courseName;
-    const updated=req.body;
-    const result =await Collection.updateOne({course:coursed},{$set:updated});
+app.patch('/students/cousre/:courseName', async (req, res) => {
+    const coursed = req.params.courseName;
+    const updated = req.body;
+    const result = await Collection.updateOne({ course: coursed }, { $set: updated });
     res.json(result);
 })
 
 
+
+
+// app.get('/studentsall',async(req,res)=>{
+//     try{
+//         const courseName=req.query.course;
+//         const data=await Collection.find({courseName:course});
+//         res.json(data);
+//     }
+//     catch(error){
+//         res.status(404).send(error)
+//     }
+// })
+
+
+
+
+//
+app.get('/studentsall', async (req, res) => {
+    try {
+        const courseName = req.query.course;
+
+        const data = await Collection.find({ course: courseName });
+
+        res.json(data);
+    }
+    catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+
+
+
+
+
+app.get('/filter',async(req,res)=>{
+    const {course,role,age}=req.query;
+
+    let filter={};
+
+    if(course){
+        filter.course=course;
+    }
+
+    if(role){
+        filter.role=role;
+    }
+
+    if(age){
+        filter.age=Number(age);
+    }
+
+    const data=await Collection.find(filter);
+    res.json(data);
+})
+
+
+
+app.get('/pagination',async (req,res)=>{
+    const page=parseInt(req.query.page)||1;
+    const limit=parseInt(req.query.limit)||10;
+    const skip=(page-1)*limit;
+
+    const data=await Collection.find().skip(skip).limit(limit);
+    req.json(data);
+})
+
+
+app.get('/pagination',async(req,res)=>{
+    const page=parseInt(req.query.page);
+    const limit=parseInt(req.query.limit);
+    const skip=(page-1)*limit;
+
+    const data=await Collection.find().skip(skip).limit(limit);
+    req.json(data);
+})
+
+
+
+
+app.get('/search', async (req, res) => {
+    try {
+
+        const { name, email, course } = req.query;
+
+        let filter = {};
+
+        if (name) {
+            filter.name = { $regex: name, $options: "i" };
+        }
+
+        if (email) {
+            filter.email = { $regex: email, $options: "i" };
+        }
+
+        if (course) {
+            filter.course = { $regex: course, $options: "i" };
+        }
+
+        const data = await Collection.find(filter);
+
+        res.json(data);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 
 
@@ -111,3 +222,4 @@ app.patch('./students/cousre/:courseName',async(req,res)=>{
 app.listen(4100, () => {
     console.log("Server Running at 4100 port");
 })
+
